7e31e2c99bb6ea65e22f7974758f927dc350877b
John Panos <johncpanos@gmail.com>
2018-07-20 22:46:53 -0700

    First commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <v-btn v-on:click="counter = counter + 1">{{ counter }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      text: 'This is an about page nerd',
+      counter: 0
+    }
+  }
+}
+</script>
